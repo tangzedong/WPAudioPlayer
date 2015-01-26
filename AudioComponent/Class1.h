9fe1fb3054e662684f785873a6e6541e14dd6f15
tangzedong <tangzedong@gmail.com>
2015-01-26 12:27:55 +0800

    add commit

---
+++ AudioComponent/Class1.h
@@ -0,0 +1,10 @@
+﻿#pragma once
+
+namespace AudioComponent
+{
+    public ref class Class1 sealed
+    {
+    public:
+        Class1();
+    };
+}
---
+++ Lib1/pch.h
@@ -0,0 +1,15 @@
+﻿//
+// pch.h
+// 标准系统包含文件的标头。
+//
+
+#pragma once
+
+#include "targetver.h"
+
+#ifndef WIN32_LEAN_AND_MEAN
+#define WIN32_LEAN_AND_MEAN             // 从 Windows 头文件中排除极少使用的内容
+#endif
+
+// Windows 头文件: 
+#include <windows.h>
